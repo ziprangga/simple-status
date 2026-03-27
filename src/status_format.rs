a0df934d552cb9c8289c1786b8bbae3afd94abcf
ziprangga <ziprangga@gmail.com>
2026-03-27 20:58:08 +0700

    refactor some format

--- src/status_format.rs
+++ src/status_format.rs
@@ -14,45 +14,47 @@ where
 }
 
 pub struct StatusFormatConfig {
-    pub parts: Vec<Box<dyn Fn(&StatusEvent) -> Option<String>>>,
+    pub field: Vec<Box<dyn Fn(&StatusEvent) -> Option<String>>>,
     pub separator: Option<String>,
 }
 
 impl StatusFormatConfig {
     pub fn new() -> Self {
         Self {
-            parts: Vec::new(),
+            field: Vec::new(),
             separator: None,
         }
     }
 
+    fn parts<F>(&mut self, f: F)
+    where
+        F: 'static + Fn(&StatusEvent) -> Option<String>,
+    {
+        self.field.push(Box::new(f));
+    }
+
     pub fn stage<F: 'static + Fn(&str) -> String>(&mut self, fmt: F) -> &mut Self {
-        self.parts
-            .push(Box::new(move |s: &StatusEvent| s.stage().map(|v| fmt(v))));
+        self.parts(move |s: &StatusEvent| s.stage().map(|v| fmt(v)));
         self
     }
 
     pub fn current<F: 'static + Fn(usize) -> String>(&mut self, fmt: F) -> &mut Self {
-        self.parts
-            .push(Box::new(move |s: &StatusEvent| s.current().map(|v| fmt(v))));
+        self.parts(move |s: &StatusEvent| s.current().map(|v| fmt(v)));
         self
     }
 
     pub fn total<F: 'static + Fn(usize) -> String>(&mut self, fmt: F) -> &mut Self {
-        self.parts
-            .push(Box::new(move |s: &StatusEvent| s.total().map(|v| fmt(v))));
+        self.parts(move |s: &StatusEvent| s.total().map(|v| fmt(v)));
         self
     }
 
     pub fn message<F: 'static + Fn(&str) -> String>(&mut self, fmt: F) -> &mut Self {
-        self.parts
-            .push(Box::new(move |s: &StatusEvent| s.message().map(|v| fmt(v))));
+        self.parts(move |s: &StatusEvent| s.message().map(|v| fmt(v)));
         self
     }
 
     pub fn path<F: 'static + Fn(&std::path::Path) -> String>(&mut self, fmt: F) -> &mut Self {
-        self.parts
-            .push(Box::new(move |s: &StatusEvent| s.path().map(|v| fmt(v))));
+        self.parts(move |s: &StatusEvent| s.path().map(|v| fmt(v)));
         self
     }
 
@@ -62,10 +64,10 @@ impl StatusFormatConfig {
     }
 
     pub fn write(&self, status: &StatusEvent) -> String {
-        let parts: Vec<String> = self.parts.iter().filter_map(|f| f(status)).collect();
+        let field: Vec<String> = self.field.iter().filter_map(|f| f(status)).collect();
         match &self.separator {
-            Some(sep) => parts.join(sep),
-            None => parts.concat(),
+            Some(sep) => field.join(sep),
+            None => field.concat(),
         }
     }
 }
